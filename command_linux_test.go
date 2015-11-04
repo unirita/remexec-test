95ccfd345496a24b87818691595b8f012ef5435d
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-11-04 16:41:26 +0900

    Add test case.
    To test remote command execution with parameter.

--- command_linux_test.go
+++ command_linux_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/unirita/remexec-test/capturer"
 )
 
-func TestCommand_NoOption(t *testing.T) {
+func TestCommand_NoParameter(t *testing.T) {
 	c := capturer.NewStdoutCapturer()
 	c.Start()
 	rc, err := executeRemoteCommand("pwd")
@@ -22,5 +22,22 @@ func TestCommand_NoOption(t *testing.T) {
 	if !strings.HasPrefix(output, "/") {
 		t.Errorf("Output is not expected format.")
 		t.Errorf("Output: %s", output)
+	}
+}
+
+func TestCommand_WithParameter(t *testing.T) {
+	c := capturer.NewStdoutCapturer()
+	c.Start()
+	rc, err := executeRemoteCommand(`echo "testmessage"`)
+	output := c.Stop()
+
+	if err != nil {
+		t.Fatalf("Error occured: %s", err)
+	}
+	if rc != 0 {
+		t.Errorf("RC => %d, wants %d", rc, 0)
+	}
+	if !output != "testmessage" {
+		t.Errorf("Output => %s, wants %s.", output, "testmessage")
 	}
 }

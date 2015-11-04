18767a4a8d42d4f65fb4d0aec4f7481c00fb0513
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-11-04 16:32:33 +0900

    Add assertion.

--- command_linux_test.go
+++ command_linux_test.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"strings"
 	"testing"
+
+	"github.com/unirita/remexec-test/capturer"
 )
 
 func TestCommand_NoOption(t *testing.T) {
-	rc, err := executeRemoteCommand("hostname")
+	c := capturer.NewStdoutCapturer()
+	c.Start()
+	rc, err := executeRemoteCommand("pwd")
+	output := c.Stop()
+
 	if err != nil {
 		t.Fatalf("Error occured: %s", err)
 	}
 	if rc != 0 {
 		t.Errorf("RC => %d, wants %d", rc, 0)
+	}
+	if !strings.HasSuffix(output, "/") {
+		t.Errorf("Output is not expected format.")
+		t.Errorf("Output: %s", output)
 	}
 }

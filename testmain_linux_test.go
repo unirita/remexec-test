1600e7c4555e504f28fdde53a06dbee0727a2a77
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-11-04 14:15:21 +0900

    Fix testmain to add error handling.

--- testmain_linux_test.go
+++ testmain_linux_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -15,7 +16,11 @@ func TestMain(m *testing.M) {
 func realTestMain(m *testing.M) int {
 	imgPath := filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "unirita",
 		"remexec-test", "_docker", "remote")
-	img := container.CreateImage("remexec/test", imgPath)
+	img, err := container.CreateImage("remexec/test", imgPath)
+	if err != nil {
+		fmt.Println("Could not build docker image.")
+		return 1
+	}
 	defer img.Remove()
 
 	return m.Run()

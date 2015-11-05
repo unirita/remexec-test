f5597bbde5ef711c861ac11061611cb8dc17fb5e
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-11-05 14:15:10 +0900

    Marge regular test codes to one file.

--- regular_linux_test.go
+++ regular_linux_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"path/filepath"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -56,6 +58,39 @@ func TestRemote_Script(t *testing.T) {
 		t.Errorf("RC => %d, wants %d", rc, 12)
 	}
 	if !strings.Contains(output, "script=/home/passuser/test.sh") {
+		t.Errorf("Output does not contains correct script value.")
+		t.Log("Output:")
+		t.Log(output)
+	}
+	if !strings.Contains(output, "param1=test1") {
+		t.Errorf("Output does not contains correct first parameter value.")
+		t.Log("Output:")
+		t.Log(output)
+	}
+	if !strings.Contains(output, "param2=test2") {
+		t.Errorf("Output does not contains correct second parameter value.")
+		t.Log("Output:")
+		t.Log(output)
+	}
+}
+
+func TestLocalScript(t *testing.T) {
+	scriptPath := filepath.Join(baseDir, "localtest.sh")
+
+	c := capturer.NewStdoutCapturer()
+	c.Start()
+	rc, err := executeLocalScript(scriptPath + ` "test1" "test2"`)
+	output := c.Stop()
+
+	if err != nil {
+		t.Fatalf("Error occured: %s", err)
+	}
+	if rc != 23 {
+		t.Errorf("RC => %d, wants %d", rc, 23)
+	}
+
+	matcher := regexp.MustCompile(`script=/home/keyuser/tmp/\d{14}\.\d{3}\.localtest\.sh`)
+	if !matcher.MatchString(output) {
 		t.Errorf("Output does not contains correct script value.")
 		t.Log("Output:")
 		t.Log(output)
